308b9cf3911f7acb1a09c7d02ce6917fa729cc66
gregc <canugregoire@gmail.com>
2020-06-16 17:39:42 +0200

    Première page
    Création controller + vue
    Ajout annotations et twig

---
+++ src/Controller/MainController.php
@@ -0,0 +1,21 @@
+<?php
+
+// src/Controller/MainController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class MainController extends AbstractController
+{
+    /**
+      * @Route("/home")
+      */
+    public function home()
+    {
+        $message = "Hello";
+
+        return $this->render('home.html.twig', ['message' => $message]);
+    }
+}
